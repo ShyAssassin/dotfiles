07b8a90d778922af264c011452473961f098af58
[Assassin] <ShyAssassin@assassin.dev>
2025-01-28 22:53:55 +0200

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+exe() { echo "\$ $@" ; "$@" ; }
 
-host=${1:-}
-sudo nixos-rebuild switch --flake .#$host
+host=${1:-$(hostname)}
+flag=${@:2}
+
+if [ "${host:0:1}" == "-" ]; then
+  flag=${@:1}
+  host=$(hostname)
+fi
+
+if [[ $flag == *"--upgrade"* ]]; then
+  exe sudo nix flake update
+fi
+
+exe sudo nixos-rebuild switch $flag --flake .#$host
