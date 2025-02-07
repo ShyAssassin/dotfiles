c4cc01b05d863797d63f0284bd767a406c5497c9
[Assassin] <ShyAssassin@assassin.dev>
2025-02-07 03:37:05 +0200

    combine deploy into switch

--- switch.sh
+++ switch.sh
@@ -1,20 +1,59 @@
 #!/bin/sh
-exe() { echo "\$ $@" ; "$@" ; }
 
-host=${1:-$(hostname)}
+exe() {
+  if [ $dryrun -eq 1 ]; then
+    echo "\$ $@"
+    return
+  fi
+  echo "\$ $@" ; "$@" ;
+}
+
+dryrun=0
+host=${1:-"$(hostname)"}
 flag=${@:2}
 
 if [ "${host:0:1}" == "-" ]; then
-  flag=${@:1}
   host=$(hostname)
+  flag=${@:1}
+fi
+
+if [[ $flag == *"--help"* ]]; then
+  echo "Usage: switch.sh [HOST] [FLAGS]"
+  echo "  HOST: hostname of the target machine"
+  echo "  FLAGS:"
+  echo "    --deploy: deploy dotfiles"
+  echo "    --help: show this help message"
+  echo "    --dry-run: show commands without executing"
+  echo "    --upgrade: upgrade nix channels and flakes"
+  exit 0
+fi
+
+if [[ $flag == *"--dry-run"* ]]; then
+  flag=${flag/--dry-run/}
+  echo "Dry run enabled"
+  dryrun=1
 fi
 
 if [[ $flag == *"--upgrade"* ]]; then
+  exe sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+  exe sudo nix-channel --update
   exe sudo nix flake update
 fi
 
+if [[ $flag == *"--deploy"* ]]; then
+  flag=${flag/--deploy/}
+  exe cp --verbose --link --force .aliasrc ~
+  exe cp --verbose --link --force .gitconfig ~
+  exe cp --verbose --link --force .gitignore ~
+  exe cp --verbose --link --force .gitattributes ~
+  exe cp --verbose --recursive --link --force .config/ ~
+fi
+
+
 if [[ "$(uname)" == "Darwin" ]]; then
   exe sudo nix run nix-darwin -- switch $flag --flake .#$host
-else
+elif [[ -f /etc/NIXOS ]]; then
   exe sudo nixos-rebuild switch $flag --flake .#$host
+else
+  echo "Unsupported OS please upgrade packages manually"
 fi
