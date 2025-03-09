646a172bac5f066564ec3fb82beae17b0029a931
[Assassin] <ShyAssassin@assassin.dev>
2025-03-10 01:47:37 +0200

    403 the suckers

--- nixos/hosts/yukime/services/nginx.nix
+++ nixos/hosts/yukime/services/nginx.nix
@@ -1,5 +1,25 @@
 {config, lib, pkgs, ...}: {
-  services.nginx.enable = true;
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    appendHttpConfig = ''
+      server {
+        listen 80 default_server;
+        listen 443 default_server;
+        listen [::]:80 default_server;
+        listen [::]:443 default_server;
+
+        # Not the correct certificate, but its a 403 so we dont care
+        ssl_certificate_key /var/lib/acme/yukime.assassin.dev/key.pem;
+        ssl_certificate /var/lib/acme/yukime.assassin.dev/fullchain.pem;
+        ssl_trusted_certificate /var/lib/acme/yukime.assassin.dev/chain.pem;
+
+        return 403;
+      }
+    '';
+  };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   security.acme = {
@@ -15,7 +35,7 @@
     forceSSL = true;
     enableACME = true;
     locations."/" = {
-      root = "/var/www/404";
+      root = "/var/www/403";
     };
   };
 }
