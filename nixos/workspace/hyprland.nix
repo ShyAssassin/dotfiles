9f2b797e20645b2719c794aa95ded5dc48440db5
[Assassin] <ShyAssassin@assassin.dev>
2025-03-23 21:11:00 +0200

    Hyprland upgrade

--- nixos/workspace/hyprland.nix
+++ nixos/workspace/hyprland.nix
@@ -30,18 +30,27 @@ in {
   security.pam.services.ly.enableGnomeKeyring = true;
   security.pam.services.gdm.enableGnomeKeyring = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
 
   # needed for nautilus stuff
   services.gvfs.enable = true;
   programs.dconf.enable = true;
   services.gnome.sushi.enable = true;
 
+  programs.nautilus-open-any-terminal = {
+    enable = true;
+    terminal = "kitty";
+  };
+
   environment.systemPackages = with pkgs; [
     grim slurp
-    adwaita-icon-theme
     clipse wl-clipboard
+    libsForQt5.qt5ct qt6ct
     waybar dunst anyrun kitty nautilus
+    gtk-engine-murrine gnome-themes-extra
     killall xorg.xrandr libnotify playerctl
+    libsForQt5.breeze-qt5 kdePackages.breeze
     hyprpaper hypridle hyprlock hyprpicker hyprpolkitagent
+    breeze-icons adwaita-icon-theme phinger-cursors tokyonight-gtk-theme
   ];
 }
