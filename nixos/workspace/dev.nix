856f8274aa70e91616487a0f8fd4ca868260b7b7
[Assassin] <ShyAssassin@assassin.dev>
2025-05-16 19:55:52 +0200

    fix man pages

--- nixos/workspace/dev.nix
+++ nixos/workspace/dev.nix
@@ -8,11 +8,24 @@
     enableFishIntegration = true;
   };
 
+  documentation = {
+    enable = true;
+    dev.enable = true;
+    man = {
+      enable = true;
+      mandoc.enable = true;
+      generateCaches = true;
+      man-db.enable = false;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     renderdoc
     vscode imhex
     gdb lldb tracy
+    bat bat-extras.batman
     git gh lazygit github-desktop
     config.boot.kernelPackages.perf
+    linux-manual man-pages man-pages-posix
   ];
 }
