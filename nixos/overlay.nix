5b84a95071d5bcad5931e147ba3f8f15dc4922d3
[Assassin] <ShyAssassin@assassin.dev>
2025-06-04 19:29:11 +0200

    miru fixes

--- nixos/overlay.nix
+++ nixos/overlay.nix
@@ -1,18 +1,27 @@
 {inputs, ...}: {
   additions = final: _prev: import ./packages final.pkgs;
 
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
     vesktop = (prev.vesktop.override {
       withTTS = false;
       withMiddleClickScroll = true;
     });
-  };
-
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
+    bottles = (prev.bottles.override {
+      removeWarningPopup = true;
+    });
+    miru = prev.miru.overrideAttrs (oldAttrs: {
+      buildInputs = (oldAttrs.buildInputs or []) ++ [prev.makeWrapper];
+      buildCommand = (oldAttrs.buildCommand or "") + ''
+        wrapProgram $out/bin/miru --unset ELECTRON_OZONE_PLATFORM_HINT
+      '';
+    });
   };
 }
