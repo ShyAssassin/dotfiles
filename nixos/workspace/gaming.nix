bb04f2ab8bba51a8140e0314bf06eeb4a195d1f2
[Assassin] <ShyAssassin@assassin.dev>
2025-07-31 17:39:07 +0200

    Enable renice / capsysnice

--- nixos/workspace/gaming.nix
+++ nixos/workspace/gaming.nix
@@ -3,6 +3,10 @@
     enable = true;
     enableRenice = true;
     settings = {
+      general = {
+        renice = 10;
+        reaper_freq = 5;
+      };
       custom = {
         end = "${pkgs.libnotify}/bin/notify-send 'GameMode disabled'";
         start = "${pkgs.libnotify}/bin/notify-send 'GameMode enabled'";
@@ -14,10 +18,21 @@
     enable = true;
     protontricks.enable = true;
     remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+    dedicatedServer.openFirewall = false;
     localNetworkGameTransfers.openFirewall = true;
   };
 
-  programs.gamescope.enable = true;
+  programs.gamescope = {
+    enable = true;
+    # currently broken
+    capSysNice = false;
+    package = pkgs.gamescope;
+    args = ["--rt" "--force-grab-cursor"];
+  };
+
   hardware.steam-hardware.enable = true;
+  environment.systemPackages = with pkgs; [ mangohud protonplus ];
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
+  };
 }
