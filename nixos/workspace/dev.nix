2f6aa1fe21505dfc70e11b105da29f71741f073a
[Assassin] <ShyAssassin@assassin.dev>
2025-09-16 16:00:19 +0200

    Enable git lfs

--- nixos/workspace/dev.nix
+++ nixos/workspace/dev.nix
@@ -19,12 +19,19 @@
     };
   };
 
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    prompt.enable = true;
+    lfs.enablePureSSHTransfer = true;
+  };
+
   environment.systemPackages = with pkgs; [
     renderdoc
     vscode imhex
     gdb lldb tracy
     bat bat-extras.batman
-    git gh lazygit github-desktop
+    gh lazygit github-desktop
     config.boot.kernelPackages.perf
     linux-manual man-pages man-pages-posix
   ];
