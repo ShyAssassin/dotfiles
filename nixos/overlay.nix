2e3bf5e6264aa3165ebfbe0c4c55946ecf086a46
[Assassin] <ShyAssassin@assassin.dev>
2026-01-05 00:11:50 +0200

    Fix discord build

--- nixos/overlay.nix
+++ nixos/overlay.nix
@@ -16,10 +16,30 @@
 
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    vesktop = (prev.vesktop.override {
+    # vesktop = (prev.vesktop.override {
+    vesktop = (prev.vesktop.overrideAttrs (oldAttrs: {
       withTTS = false;
       withMiddleClickScroll = true;
-    });
+
+      # Can be removed once this lands in unstable
+      # https://github.com/NixOS/nixpkgs/issues/476669
+      preBuild = ''
+        cp -r ${prev.electron.dist} electron-dist
+        chmod -R u+w electron-dist
+      '';
+      buildPhase = ''
+        runHook preBuild
+
+        pnpm build
+        pnpm exec electron-builder \
+          --dir \
+          -c.asarUnpack="**/*.node" \
+          -c.electronDist="electron-dist" \
+          -c.electronVersion=${prev.electron.version}
+
+        runHook postBuild
+      '';
+    }));
 
     bottles = (prev.bottles.override {
       removeWarningPopup = true;
