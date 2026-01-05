8715eeae507ab4e1c9bca0f1c9637c00b730de3b
[Assassin] <ShyAssassin@assassin.dev>
2026-01-05 23:29:58 +0200

    Use normal nvidia drivers

--- nixos/hosts/miyabi/miyabi-hw.nix
+++ nixos/hosts/miyabi/miyabi-hw.nix
@@ -26,14 +26,14 @@
     powerManagement.finegrained = false;
     # https://github.com/NixOS/nixpkgs/pull/458794
     # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/960
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "580.95.05";
-      openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-      sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-      sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-      settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-      persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-    };
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "580.95.05";
+    #   openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+    #   sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    #   sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+    #   settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+    #   persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+    # };
   };
 
   fileSystems."/boot" = {
