4da48b707c870541dc393bdff3364b1b006599fa
[Assassin] <ShyAssassin@assassin.dev>
2026-08-16 10:10:00 +0200

    Hyprland bump

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     devnotify.url = "github:ShyAssassin/devnotify";
 
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.55.4";
+      url = "github:hyprwm/Hyprland?ref=v0.56.2";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
