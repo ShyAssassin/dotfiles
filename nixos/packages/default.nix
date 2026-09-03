a6103800939defb234ec06073b9493956908e2a7
[Assassin] <ShyAssassin@assassin.dev>
2026-09-04 01:09:21 +0200

    Package sable-bin

--- nixos/packages/default.nix
+++ nixos/packages/default.nix
@@ -1,5 +1,6 @@
 {inputs, pkgs, ...}: rec {
   vrcx-bin = pkgs.callPackage ./vrcx-bin.nix { };
-  scopebuddy = pkgs.callPackage ./scopebuddy.nix {};
+  sable-bin = pkgs.callPackage ./sable-bin.nix { };
+  scopebuddy = pkgs.callPackage ./scopebuddy.nix { };
   easymotion = pkgs.callPackage ./easymotion.nix { inherit inputs; };
 }
---
+++ nixos/packages/sable-bin.nix
@@ -0,0 +1,42 @@
+{
+  fetchurl,
+  lib, libGL,
+  appimageTools,
+}: let
+  version = "1.21.0";
+  pname = "sable-bin";
+  src = fetchurl {
+    hash = "sha256-dAQErvyoZ9rfrvpgdKVLcr0isEN+B92LE6J0wB99xhY=";
+    url = "https://github.com/SableClient/Sable/releases/download/v${version}/Sable-${version}-linux-x86_64.AppImage";
+  };
+  appimageContents = appimageTools.extract {
+    inherit pname src version;
+  };
+in appimageTools.wrapType2 rec {
+  inherit pname version src;
+  extraPkgs = pkgs: [ libGL ];
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/sable.png \
+      $out/share/icons/hicolor/128x128/apps/sable.png
+    install -m 444 -D ${appimageContents}/sable.desktop \
+      $out/share/applications/sable.desktop
+
+    # no clue why wayland is broken, so force x11 for now
+    substituteInPlace $out/share/applications/sable.desktop \
+      --replace-fail 'Exec=sable %U' 'Exec=${pname} --no-install --ozone-platform-hint=x11 --no-desktop'
+  '';
+
+  meta = {
+    description = "An almost stable Matrix client.";
+    longDescription = ''
+      A Matrix client built to enhance the user experience with quality-of-life features, cosmetics, utilities, and sheer usability.
+    '';
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    homepage = "https://github.com/SableClient/Sable";
+    maintainers = with lib.maintainers; [ ShyAssassin ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    downloadPage = "https://github.com/SableClient/Sable/releases";
+  };
+}
